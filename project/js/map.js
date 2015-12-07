86bef5cc6cb5191a6aa1ed110353bdb441423c8c
Christoph Kisfeld <spam@b-web.org>
2015-12-07 14:52:46 +0100

    Basic map

---
+++ project/js/map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([51.9609808, 7.62416839], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
