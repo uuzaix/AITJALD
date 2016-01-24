3a153e35eade55b8b695ab52e92e0294a7d8cc9f
Florian Lahn <florian.lahn@uni-muenster.de>
2016-01-24 14:53:46 +0100

    - reworked the object handling to be more reusable - introduced GLOBAL flags to prevent reloading the documentation - refined the query - added code documentation for functions

--- project/js/doc.js
+++ project/js/doc.js
@@ -1,10 +1,13 @@
-hideDocumentation();
 
-$('#doc_tab').on('click', function(){
-    
+
+$('#voc_tab').on('click', function(){
+
+    if (!GLOBAL.DOCUMENTATION_FETCHED) {
+        $("#voc").empty();
+        queryVocabulary();
+    }
     hideMap();
     showDocumentation();
-    queryVocabulary();
 });
 
 var Prefixes = {
@@ -12,7 +15,7 @@ var Prefixes = {
      * The used prefixes as a key-value pair JSON object 
      */
     vocabularies : {
-       "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+        "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
         "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
         "sf": "http://www.opengis.net/ont/sf#",
         "time": "http://www.w3.org/2006/time#",
@@ -54,13 +57,11 @@ var Prefixes = {
      */
     replaceURIs: function(s, sel) {
         var out = s;
-        console.log(typeof s);
         for (var i = 0; i < sel.length; i++) {
             var key = sel[i];
             var uri = this.vocabularies[key];
             if (uri) {
                 out = out.replace(new RegExp(uri,'g'),key+":");
-                //out = out.replace(uri,key+":");
             }
         }
         return out;
@@ -74,41 +75,147 @@ var Prefixes = {
 var QUERY_DOC = "SELECT DISTINCT ?x ?prop ?val\r\n" + 
             "WHERE {\r\n" + 
             "   GRAPH <http://course.introlinkeddata.org/G4> {\r\n" + 
-            "       {\r\n" + 
-            "           { \r\n" + 
-            "               ?x a rdf:Property.\r\n" + 
-            "           } UNION {\r\n" + 
-            "               ?x rdfs:subPropertyOf ?y.\r\n" + 
-            "           }\r\n" + 
+            "       { \r\n" + 
+            "           ?x a rdf:Property.\r\n" + 
             "       } UNION {\r\n" + 
-            "           { \r\n" + 
-            "               ?x a rdf:Class.\r\n" + 
-            "           } UNION {\r\n" + 
-            "               ?x rdfs:subClassOf ?y.\r\n" + 
-            "           }\r\n" + 
+            "           ?x rdfs:subPropertyOf ?y.\r\n" + 
+            "       } UNION {\r\n" + 
+            "           ?x a rdfs:Class.\r\n" + 
+            "       } UNION {\r\n" + 
+            "           ?x rdfs:subClassOf ?y.\r\n" + 
+            "       } UNION {\r\n" + 
+            "           ?x a qb:DataSet.\r\n" + 
+            "       } UNION {\r\n" + 
+            "           ?x a qb:DataStructureDefinition.\r\n" + 
             "       }\r\n" + 
+            "\r\n" + 
             "       \r\n" + 
             "       ?x ?prop ?val.\r\n" + 
             "   }\r\n" + 
             "} ORDER BY ?x";
 
+/**
+ * Queries the parliament server for the vocabulary information by sending a HTTP POST request. The
+ * data returned will be in the JSON format. 
+ */
 function queryVocabulary() {
     $.post("http://giv-lodumdata.uni-muenster.de:8282/parliament/sparql", {
         query: Prefixes.toString()+QUERY_DOC,
         output: 'json'
     },
     function(data){
-        console.log(data);
-        visualizeData(data);
+        //console.log(data);
+        visualizeVocabularyData(data);
+        GLOBAL.DOCUMENTATION_FETCHED = true;
     });
 }
 
-function visualizeData(data) {
+/**
+ * This callback function will be used to visualize the returning vocabulary data, that was created
+ * during the course. It will create different div blocks containing a title, a description and the
+ * assigned Property-Value statements.
+ *  
+ * @param {Object} data The returning JSON data bindings from the parliament server.
+ */
+function visualizeVocabularyData(data) {
     var topic = data.results.bindings[0].x.value;
+    var entry = new VocabularyEntry();
+    entry.setTitle(topic);
     
+    for (var i = 0; i < data.results.bindings.length; i++) {
+        var cursor = data.results.bindings[i];
+        if (topic == cursor.x.value) {
+            entry.addData(cursor.prop,cursor.val);
+        } else {
+            entry.appendTo("#voc");
+            
+            entry = new VocabularyEntry();
+            topic = cursor.x.value;
+            entry.setTitle(topic);
+            entry.addData(cursor.prop,cursor.val);
+        }
+    }
+    entry.appendTo("#voc");
+}
+
+/**
+ * A template that will be constructed using the function 'VocabularyEntry'. This object
+ * will organize the data structure of each entry of the vocabulary. 
+ */
+var VocabularyEntryTemplate = {
+    /** the parent node of the entry */
+    entryDiv: null,
+    /** a link containing the URI as title for the block */
+    title: null,
+    /** a div containing the description (rdfs:comment) */
+    commentDiv: null,
+    /** the parent div of the properties tabele */
+    tableDiv: null,
+    /** the table structure */
+    table: null,
+    
+    /**
+     * A function that sets the title of the block as well as the ID.
+     * 
+     * @param {string} uri The URI of the element as a string.
+     */
+    setTitle: function(uri) {
+        this.entryDiv.attr("id",uri);
+        this.title.text(uri).attr("href",uri).attr("target","_blank");
+    },
+    /**
+     * Setting the description.
+     * 
+     * @param {string} comment The description as a string obtained from rdfs:comment statement. 
+     */
+    setComment: function (comment) {
+        this.commentDiv.text(comment);
+    },
+    /**
+     * Function to add property and values to the entry. If the property is rdfs:comment, then it
+     * will be assigned as the description otherwise it will be added to the property table. The input
+     * objects can be derived from the JSON bindings received from the server for the used SparQL query.
+     * 
+     * @param {Object} propObj The object for variable 'prop' of the JSON bindings.
+     * @param {Object} valObj The object for variable 'val' of the JSON bindings.
+     */
+    addData: function (propObj, valObj) {
+        var property = evalType(propObj);
+        var value = evalType(valObj);
+        if (propObj.value == Prefixes.vocabularies["rdfs"]+"comment") {
+            this.setComment(value);
+        } else {
+            var new_row = $("<tr>");
+            var prop_td = $("<td>").append(property);
+            var val_td = $("<td>").append(value);
+            new_row.append(prop_td).append(val_td);
+            
+            this.table.append(new_row);
+        }
+    },
+    /**
+     * Appends this entry to a parent by stating the parents id (JQuery notion).
+     * 
+     * @param {string} idString The id of the parent element in JQuery notion, e.g. "#parent".
+     */
+    appendTo: function (idString) {
+        this.entryDiv.append("<br/>");
+        this.entryDiv.appendTo(idString);
+    }
+};
+
+/**
+ * Constructor for the div data block namely the vocabulary entry.
+ * 
+ * @return {Object} A new vocabulary entry object.
+ */
+function VocabularyEntry() {
     var topic_div = $("<div>");
     var title_div = $("<div>");
-    title_div.append("<h2><a>"+topic+"</a><h2>");
+    var title = $("<a>");
+    var title_h2 = $("<h2>");
+    title_div.append(title_h2);
+    title_h2.append(title);
     topic_div.append(title_div);
     var comment_div = $("<div>");
     topic_div.append(comment_div);
@@ -119,82 +226,27 @@ function visualizeData(data) {
     table_div.append(table);
     topic_div.append(table);
     
-    for (var i = 0; i < data.results.bindings.length; i++) {
-        var cursor = data.results.bindings[i];
-        if (topic == cursor.x.value) {
-            //add entry into table
-            var property = evalType(cursor.prop);
-            var value = evalType(cursor.val);
-            if (property == Prefixes.vocabularies["rdfs"]+"comment") {
-                comment_div.text(value);
-            } else {
-                var new_row = $("<tr>");
-                var prop_td = $("<td>").append(property);
-                var val_td = $("<td>").append(value);
-                new_row.append(prop_td).append(val_td);
-                
-                table.append(new_row);
-            }
-        } else {
-            //table.html(Prefixes.replaceURIs(table.html(),["rdf","rdfs","lodcom"]));
-            $("#doc").append(topic_div);
-            $("#doc").append("<br/>");
-            topic = cursor.x.value;
-            
-            //next block
-            topic_div = $("<div>");
-            title_div = $("<div>");
-            title_div.append("<h2><a>"+topic+"</a><h2>");
-            topic_div.append(title_div);
-            comment_div = $("<div>");
-            topic_div.append(comment_div);
-            table_div = $("<div>").addClass("table-responsive");
-            table = $("<table>").addClass("table").addClass("table-striped");
-            header = $("<tr><th>Property</th><th>Value</th></tr>");
-            table.append(header);
-            table_div.append(table);
-            topic_div.append(table);
-            
-            var property = evalType(cursor.prop);
-            var value = evalType(cursor.val);
-            if (property == Prefixes.vocabularies["rdfs"]+"comment") {
-                comment_div.text(value);
-            } else {
-                var new_row = $("<tr>");
-                var prop_td = $("<td>").append(property);
-                var val_td = $("<td>").append(value);
-                new_row.append(prop_td).append(val_td);
-                
-                table.append(new_row);
-            }
-        }
-    }
-    //table.html(Prefixes.replaceURIs(table.html(),["rdf","rdfs","lodcom"]));
-    $("#doc").append(topic_div);
+    var obj = VocabularyEntryTemplate;
+    obj.entryDiv = topic_div;
+    obj.title = title;
+    obj.commentDiv = comment_div;
+    obj.tableDiv = table_div;
+    obj.table = table;
+    
+    return obj;
 }
 
+/**
+ * Simple evaluation function for the JSON bindings object. It will create links from URIs.
+ *  
+ * @param {Object} obj One object of the JSON bindings from the query result.
+ */
 function evalType(obj) {
-    console.log(obj.type);
     if (obj.type == "uri") {
         var uri = $("<a>", {href: obj.value, text:obj.value, target:"_blank"});
         return uri;
     } else {
         return obj.value;
     }
-}
-
-function visualizeData_old(data) {
-    var new_table = $("<table>",{id: "doc_results"});
-    var header = $("<tr><th>Vocabulary</th><th>Property</th><th>Value</th></tr>");
-    new_table.append(header);
-    
-    for (var i = 0; i < data.results.bindings.length; i++) {
-        var cursor = data.results.bindings[i];
-        var new_row = $("<tr><td>"+cursor.x.value+"</td><td>"+cursor.prop.value+"</td><td>"+cursor.val.value+"</td></tr>");
-        new_table.append(new_row);
-    }
-    //console.log();
-    //new_table.html()
-    $("#doc").append(Prefixes.replaceURIs(new_table.html(),["rdf","rdfs","lodcom"]));
 }
 
